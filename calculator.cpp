34dc0c778d19de95d04bfd9cee81817de0f24b07
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:21:34 +0200

    improve calculation accuracy
    Allow using float point arguments to avoid truncation.
    
    Test: Add (4.9, 4.9) should return 10.
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>
    
    fix truncation error
    
    To convert float to integer the truncation is performed, but the
    rounding is expected.
    
    Test: Add (4.9, 4.9) should return 10.
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>

--- calculator.cpp
+++ calculator.cpp
@@ -1,11 +1,9 @@
 #include "calculator.h"
 
-int Calculator::Add (int a, int b)
+int Calculator::Add (double a, double b)
 {
-    return a + b;
-}
-
-int Calculator::Sub (int a, int b)
+	return a + b + 0.5;
+int Calculator::Sub (double a, double b)
 {
-    return Add (a, -b);
+    	return Add (a, -b);
 }
